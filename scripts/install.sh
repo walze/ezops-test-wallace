1fbbcd63c5ce3ed735e40b3f6bbd29b612b80b6d
walze <catufuzgu@gmail.com>
2020-08-01 14:08:38 -0300

    fixed apt-getr

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,8 +2,8 @@
 cd /tmp/
 
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
-sudo apt-get install -y nodejs
-sudo apt-get install -y build-essential
+sudo yum install -y nodejs
+sudo yum install -y build-essential
 
 npm install -g pm2
 npm install
