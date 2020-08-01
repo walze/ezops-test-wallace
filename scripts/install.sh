b7c9abeea302f403cc48f1d26a8f85e324198983
walze <catufuzgu@gmail.com>
2020-08-01 15:10:33 -0300

    added npm install pm2

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,6 +6,5 @@ curl -sL https://rpm.nodesource.com/setup_12.x | sudo -E bash -
 sudo yum install -y build-essential
 sudo yum install -y nodejs
 
-wget -qO- https://getpm2.com/install.sh | bash
-
+npm install pm2 -g
 npm install
