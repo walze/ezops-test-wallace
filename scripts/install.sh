5878146d3816cc58a10ce368fbaea108b740e8d6
walze <catufuzgu@gmail.com>
2020-08-01 15:08:50 -0300

    test

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,8 +3,8 @@ cd /tmp/
 
 sudo yum install -y gcc-c++ make
 curl -sL https://rpm.nodesource.com/setup_12.x | sudo -E bash -
-sudo yum install -y nodejs
 sudo yum install -y build-essential
+sudo yum install -y nodejs
 
 wget -qO- https://getpm2.com/install.sh | bash
 
