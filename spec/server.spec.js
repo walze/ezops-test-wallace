c52e96e3ce11dc1f1f20bf797364f1ee147dd786
walze <catufuzgu@gmail.com>
2020-08-02 10:33:38 -0300

    removed forced test error

--- spec/server.spec.js
+++ spec/server.spec.js
@@ -3,13 +3,13 @@ var request = require('request')
 
 describe('get messages', () => {
     it('should return 200 Ok', (done) => {
-        request.get('http://alocalhost:3000/messages', (err, res) => {
+        request.get('http://localhost:3000/messages', (err, res) => {
             expect(res.statusCode).toEqual(200)
             done()
         })
     })
     it('should return a list, thats not empty', (done) => {
-        request.get('http://alocalhost:3000/messages', (err, res) => {
+        request.get('http://localhost:3000/messages', (err, res) => {
             expect(JSON.parse(res.body).length).toBeGreaterThan(0)
             done()
         })
@@ -19,13 +19,13 @@ describe('get messages', () => {
 
 describe('get messages from particular user', () => {
     it('should return 200 Ok', (done) => {
-        request.get('http://alocalhost:3000/messages/Arun', (err, res) => {
+        request.get('http://localhost:3000/messages/Arun', (err, res) => {
             expect(res.statusCode).toEqual(200)
             done()
         })
     })
     it('should return the message of the user', (done) => {
-        request.get('http://alocalhost:3000/messages/Arun', (err, res) => {
+        request.get('http://localhost:3000/messages/Arun', (err, res) => {
             expect(JSON.parse(res.body)[0].name).toEqual('Arun')
             done()
         })
